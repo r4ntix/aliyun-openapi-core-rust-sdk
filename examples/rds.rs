cbe4fa960e18a5a7604bb63d6fe56ae06cc096df
r.4ntix <r.4ntix@gmail.com>
2023-05-22 22:20:19 +0800

    chore: update RPC style client examples

--- examples/rds.rs
+++ examples/rds.rs
@@ -1,25 +1,32 @@
-use aliyun_openapi_core_rust_sdk::RPClient;
+use aliyun_openapi_core_rust_sdk::client::rpc::RPClient;
 use std::env;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // create rpc style api client.
     let aliyun_openapi_client = RPClient::new(
         env::var("ACCESS_KEY_ID")?,
         env::var("ACCESS_KEY_SECRET")?,
-        String::from("https://rds.aliyuncs.com/"),
-        String::from("2014-08-15"),
+        "https://rds.aliyuncs.com/",
     );
 
     // call `DescribeRegions` with empty queries.
-    let response = aliyun_openapi_client.get("DescribeRegions").send()?;
+    let response = aliyun_openapi_client
+        .clone()
+        .version("2014-08-15")
+        .get("DescribeRegions")
+        .text()
+        .await?;
     println!("DescribeRegions response: {}", response);
 
     // call `DescribeDBInstances` with queries.
     let response = aliyun_openapi_client
+        .version("2014-08-15")
         .get("DescribeDBInstances")
-        .query(&[("RegionId", "cn-hangzhou")])
-        .send()?;
+        .query([("RegionId", "cn-hangzhou")])
+        .text()
+        .await?;
     println!("DescribeDBInstances response: {}", response);
 
     Ok(())
--- examples/vpc.rs
+++ examples/vpc.rs
@@ -1,25 +1,32 @@
-use aliyun_openapi_core_rust_sdk::RPClient;
+use aliyun_openapi_core_rust_sdk::client::rpc::RPClient;
 use std::env;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // create rpc style api client.
     let aliyun_openapi_client = RPClient::new(
         env::var("ACCESS_KEY_ID")?,
         env::var("ACCESS_KEY_SECRET")?,
-        String::from("https://vpc.aliyuncs.com/"),
-        String::from("2016-04-28"),
+        "https://vpc.aliyuncs.com/",
     );
 
     // call `DescribeRegions` with empty queries.
-    let response = aliyun_openapi_client.get("DescribeRegions").send()?;
+    let response = aliyun_openapi_client
+        .clone()
+        .version("2016-04-28")
+        .get("DescribeRegions")
+        .text()
+        .await?;
     println!("DescribeRegions response: {}", response);
 
     // call `DescribeVpcs` with queries.
     let response = aliyun_openapi_client
+        .version("2016-04-28")
         .get("DescribeVpcs")
-        .query(&[("RegionId", "cn-hangzhou")])
-        .send()?;
+        .query([("RegionId", "cn-hangzhou")])
+        .text()
+        .await?;
     println!("DescribeVpcs response: {}", response);
 
     Ok(())
