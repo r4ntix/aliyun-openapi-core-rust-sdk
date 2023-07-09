191af8e6a5c0e561cbb5286b1f9c8689c9ae4917
r.4ntix <r.4ntix@gmail.com>
2023-07-09 11:43:11 +0800

    docs(example): update ros example

--- examples/ros.rs
+++ examples/ros.rs
@@ -1,6 +1,21 @@
 use aliyun_openapi_core_rust_sdk::client::roa::ROAClient;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::error::Error;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Region {
+    region_id: String,
+    region_endpoint: String,
+    local_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct RegionList {
+    regions: Vec<Region>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -15,10 +30,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let response = aliyun_openapi_client
         .version("2015-09-01")
         .get("/regions")
-        .text()
+        .json::<RegionList>()
         .await?;
 
-    println!("DescribeRegions response:\n{response}");
+    println!("DescribeRegions response: {response:#?}");
 
     Ok(())
 }
