7e0ba9c3f5a968d7522145ab3bf952f253ea4e38
r.4ntix <r.4ntix@gmail.com>
2023-07-09 22:45:55 +0800

    docs(example): update vpc example

--- examples/vpc.rs
+++ examples/vpc.rs
@@ -1,6 +1,35 @@
-use aliyun_openapi_core_rust_sdk::client::rpc::RPClient;
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
+
+use aliyun_openapi_core_rust_sdk::client::rpc::RPClient;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Region {
+    region_id: String,
+    region_endpoint: String,
+    local_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct RegionList {
+    request_id: String,
+    regions: HashMap<String, Vec<Region>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct VpcList {
+    request_id: String,
+    total_count: usize,
+    vpcs: HashMap<String, Vec<Value>>,
+    page_size: usize,
+    page_number: usize,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -11,23 +40,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "https://vpc.aliyuncs.com/",
     );
 
-    // call `DescribeRegions` with empty queries.
+    // call `DescribeRegions` with empty queries, return `RegionList`
     let response = aliyun_openapi_client
         .clone()
         .version("2016-04-28")
         .get("DescribeRegions")
-        .text()
+        .json::<RegionList>()
         .await?;
-    println!("DescribeRegions response:\n{response}\n");
+    println!("DescribeRegions response: {response:#?}");
 
-    // call `DescribeVpcs` with queries.
+    // call `DescribeVpcs` with queries, return `VpcList`
     let response = aliyun_openapi_client
         .version("2016-04-28")
         .get("DescribeVpcs")
         .query([("RegionId", "cn-hangzhou")])
-        .text()
+        .json::<VpcList>()
         .await?;
-    println!("DescribeVpcs response:\n{response}");
+    println!("DescribeVpcs response: {response:#?}");
 
     Ok(())
 }
