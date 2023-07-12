fccb324c6dd81dc4d619b88214bb98284eeda6f9
r.4ntix <r.4ntix@gmail.com>
2023-07-12 22:04:43 +0800

    docs(example): update slb example

--- examples/slb.rs
+++ examples/slb.rs
@@ -1,6 +1,34 @@
-use aliyun_openapi_core_rust_sdk::client::rpc::RPClient;
-use std::env;
 use std::error::Error;
+use std::{collections::HashMap, env};
+
+use aliyun_openapi_core_rust_sdk::client::rpc::RPClient;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Region {
+    region_id: String,
+    region_endpoint: String,
+    local_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct RegionList {
+    request_id: String,
+    regions: HashMap<String, Vec<Region>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct SlbList {
+    request_id: String,
+    total_count: usize,
+    load_balancers: HashMap<String, Vec<Value>>,
+    page_size: usize,
+    page_number: usize,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -11,23 +39,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "https://slb.aliyuncs.com/",
     );
 
-    // call `DescribeRegions` with empty queries.
+    // call `DescribeRegions` with empty queries, return `RegionList`
     let response = aliyun_openapi_client
         .clone()
         .version("2014-05-15")
         .get("DescribeRegions")
-        .text()
+        .json::<RegionList>()
         .await?;
-    println!("DescribeRegions response:\n{response}\n");
+    println!("DescribeRegions response: {response:#?}");
 
-    // call `DescribeLoadBalancers` with queries.
+    // call `DescribeLoadBalancers` with queries, return `SlbList`
     let response = aliyun_openapi_client
         .version("2014-05-15")
         .get("DescribeLoadBalancers")
         .query([("RegionId", "cn-hangzhou")])
-        .text()
+        .json::<SlbList>()
         .await?;
-    println!("DescribeLoadBalancers response:\n{response}");
+    println!("DescribeLoadBalancers response: {response:#?}");
 
     Ok(())
 }
