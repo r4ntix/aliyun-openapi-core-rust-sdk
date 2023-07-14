ec65ef60b76a80bb6f00044c54b347ad84969af2
r.4ntix <r.4ntix@gmail.com>
2023-07-14 21:48:53 +0800

    docs(example): update sc example

--- examples/sc.rs
+++ examples/sc.rs
@@ -1,6 +1,20 @@
-use aliyun_openapi_core_rust_sdk::client::roa::ROAClient;
-use std::env;
 use std::error::Error;
+use std::{collections::HashMap, env};
+
+use aliyun_openapi_core_rust_sdk::client::roa::ROAClient;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct ClusterList {
+    clusters: HashMap<String, Vec<Value>>,
+    page_index: usize,
+    page_size: usize,
+    request_id: String,
+    total_count: usize,
+    total_page: usize,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -15,10 +29,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let response = aliyun_openapi_client
         .version("2018-11-11")
         .get("/api/v2/clusters")
-        .text()
+        .json::<ClusterList>()
         .await?;
 
-    println!("ListCluster response:\n{response}");
+    println!("ListCluster response: {response:#?}");
 
     Ok(())
 }
